29ff9f88d995fe94f85b584a2ddd6fb348f45e3e
GetanehYilma <gyletike@mum.edu>
2019-09-11 12:22:18 -0500

    second commit

--- scopefuncs.js
+++ scopefuncs.js
@@ -1,9 +1,5 @@
 
-// function filter(arrayString){
-//     String.prototype.
-//
-// }
-
+//Exercise 1
 String.prototype.filter = function (badWord) {
     const splitedString = this.split(" ");
 
@@ -15,5 +11,21 @@ String.prototype.filter = function (badWord) {
     return filtered.reduce(function(prevValue, elem, i, array) {
         return prevValue + " " + elem;
     });
-}
+};
 console.log("This house is not nice!".filter('not'));
+
+//Exercise 2:
+Array.prototype.bubbleSort = function(){
+    // const temp = this[0];
+
+    for(let i = 0; i < this.length; i++){
+        if (this[i+1] < this[i]){
+            let temp = this[i];
+              this[i] = this[i+1];
+              this[i+1] = temp;
+        }
+
+    }
+    return this;
+};
+console.log([6,4,0, 3,-2,1].bubbleSort());
