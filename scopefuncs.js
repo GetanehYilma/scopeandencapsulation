7d697c1318c2035a9ff3fdcb9be277839f3eb4d9
GetanehYilma <gyletike@mum.edu>
2019-09-11 12:29:49 -0500

    second commit

--- scopefuncs.js
+++ scopefuncs.js
@@ -18,14 +18,25 @@ console.log("This house is not nice!".filter('not'));
 Array.prototype.bubbleSort = function(){
     // const temp = this[0];
 
-    for(let i = 0; i < this.length; i++){
-        if (this[i+1] < this[i]){
-            let temp = this[i];
-              this[i] = this[i+1];
-              this[i+1] = temp;
-        }
+    // for(let i = 0; i < this.length; i++){
+    //     if (this[i+1] < this[i]){
+    //         let temp = this[i];
+    //           this[i] = this[i+1];
+    //           this[i+1] = temp;
+    //     }
+    //
+    // }
 
+    for(let i = 0; i < this.length; i++){
+        for (let j = i+1; j < this.length; j++){
+            if (this[j] < this[i]){
+                let temp = this[i];
+                this[i] = this[j];
+                this[j] = temp;
+            }
+        }
     }
+
     return this;
 };
 console.log([6,4,0, 3,-2,1].bubbleSort());
